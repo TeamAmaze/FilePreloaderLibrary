eb4ee3f20047f4ad44a8c7068d4815e3132a0af2
Emmanuel M <emmanuelbendavid@gmail.com>
2018-04-10 16:08:22 -0300

    JNI now runs with -std=c++17 and added liblog.so

--- lib/src/main/jni/Android.mk
+++ lib/src/main/jni/Android.mk
@@ -2,6 +2,9 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_LDLIBS    := -llog
+APP_CPPFLAGS    += -std=c++17
+
 LOCAL_MODULE    := native-filesystem-functions
 LOCAL_SRC_FILES := native.c
 
