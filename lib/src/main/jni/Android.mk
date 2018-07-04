b868494d467b008737ab94eba5b496b467129002
Emmanuel M <emmanuelbendavid@gmail.com>
2018-07-04 13:55:07 -0300

    Added support to all archs and added support for exceptions

--- lib/src/main/jni/Android.mk
+++ lib/src/main/jni/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_LDLIBS    := -llog
-APP_CPPFLAGS    += -std=c++17
+APP_CPPFLAGS    += -std=c++17 -lstdc++
 
 LOCAL_MODULE    := native-filesystem-functions
 LOCAL_SRC_FILES := native.cpp
